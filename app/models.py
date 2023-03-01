4d82745e87b0c1f0f0f5ccd1461b5e93987628be
shlvd <39949334+shlvd@users.noreply.github.com>
2023-03-01 18:05:10 +0200

    add models

---
+++ app/models.py
@@ -0,0 +1,18 @@
+from sqlalchemy import TIMESTAMP, Column, String, Boolean
+from sqlalchemy.sql import func
+from fastapi_utils.guid_type import GUID, GUID_DEFAULT_SQLITE
+from .database import Base
+
+
+class Book(Base):
+    """Note model."""
+    __tablename__ = 'notes'
+    id = Column(GUID, primary_key=True, default=GUID_DEFAULT_SQLITE)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    category = Column(String, nullable=True)
+    published = Column(Boolean, nullable=False, default=True)
+    createdAt = Column(TIMESTAMP(timezone=True),
+                       nullable=False, server_default=func.now())
+    updatedAt = Column(TIMESTAMP(timezone=True),
+                       default=None, onupdate=func.now())
